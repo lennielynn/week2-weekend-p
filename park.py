3c89a18b76c1ed3773cf7e150995b4ead2f2c5d5
Lyla Guthrie <lylaguthrie68@gmail.com>
2023-09-04 17:48:53 -0400

    final commit

--- park.py
+++ park.py
@@ -11,40 +11,46 @@ class ParkingGarage:
         print(self.current_ticket)
         
     def payForTicket(self):
-        pay_ticket = input ("please enter payment amouunt and enter [purchase] ").lower()
+        pay_ticket = input ("please input payment and enter [purchase] ").lower()
         if pay_ticket == 'purchase':
             self.current_ticket["Paid"] = True
-            return self.current_ticket
+            print(self.current_ticket)
+            print('Thank you, purchase complete.')
+        else:
+            print('Please enter [purchase] to complete transaction')
+            my_ticket.payForTicket()
     
     def takeTicket(self):
-       self.tickets = self.tickets - 1
-       print(f'There are {self.tickets} tickets remaining')
-       self.parking_spaces = self.parking_spaces - 1
-       print(f'There are {self.parking_spaces} parking spaces remaining')
-       
+       if self.tickets < 1 or self.parking_spaces < 1:
+           print('Garage Full')
+       elif self.tickets  >= 1 or self.parking_spaces >= 1 : 
+           self.tickets = self.tickets - 1
+           print(f'There are {self.tickets}/300 tickets remaining')
+           self.parking_spaces = self.parking_spaces - 1   
+           print(f'There are {self.parking_spaces}/300 parking spaces remaining')
+           
     def leaveGarage(self):
-        if self.current_ticket == False:
+         if self.current_ticket['Paid'] == False:
+            print ('Your balance in unpaid. redirecting to payment..')
             my_ticket.payForTicket()
-            
-        elif self.current_ticket.values() is True:
+         elif self.current_ticket['Paid'] == True:
           self.tickets = self.tickets + 1
           self.parking_spaces = self.parking_spaces + 1
-          print(f'There are {self.parking_spaces} parking spaces remaining')
-        # print("Thank you, come again!")
-    
+          print('Thank you, have a nice day!')
+       
+       
 def driver():
         while True:
-          response = input("Pay, Enter, View, or Exit? ")
+          response = input("Pay, Enter, Leave, or View? ")
         
           if response.lower() == 'view':
              my_ticket.displayTicket()
-             break
           elif response.lower() == 'pay':
              my_ticket.payForTicket()
           elif response.lower() == 'enter':
              my_ticket.takeTicket()
-          elif response.lower() == 'exit':
-            my_ticket.leaveGarage()
+          elif response.lower() == 'leave':
+             my_ticket.leaveGarage()
             
 my_ticket = ParkingGarage(300, 300, {'Paid': False})
 
